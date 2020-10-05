36bd24d589ce8ffeeaf9215fb7c71884ff475dd7
Eric White <ew13023@gmail.com>
2020-10-05 19:59:58 -0400

    madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+noun = input("gimme a noun...")
+adjective = input("How 'about an adjective?")
+adverb = input("Can I get an adverb?")
+verb = input("And finally, how about a verb?")
+
+print("on a tropical island ")
+print("underneath a lava moon")
+print("hangin with the hula dancers")
+print("questions cause they all got the answers ")
